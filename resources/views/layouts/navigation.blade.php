26340487143d9aab5fdb94eb8ebe69fe0857243e
Jeremy Kratzer <git@pgrahamdunn.com>
2023-11-28 09:49:19 -0500

    add dev to logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,12 +3,12 @@
         <div>
             <!-- logo -->
             <div class="h-18  border-b border-gray-600">
-                <div class="p-2 bg-gray-600 m-2 rounded-md">
+            <div class="p-2 {{ env('APP_DEBUG') == 'true' ? 'bg-green-600'  : 'bg-gray-600' }} m-2 rounded-md">
                     <a class="flex items-center space-x-2" href="">
                         <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        <span class="text-2xl">{{ env('APP_NAME') }}</span>
+                        <span class="text-2xl">{{ env('APP_DEBUG') == 'true' ? env('APP_NAME') . ' - DEV'  : env('APP_NAME') }}</span>
                     </a>
                 </div>
             </div>
