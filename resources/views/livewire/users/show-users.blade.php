c73737382c010e480caad05dfd9cd4d535b8be86
Jeremy Kratzer <git@pgrahamdunn.com>
2023-12-01 08:23:18 -0500

    add button main

--- resources/views/livewire/users/show-users.blade.php
+++ resources/views/livewire/users/show-users.blade.php
@@ -15,7 +15,7 @@
                 </div>
                 <div class="flex items-start">
                     <a href="{{ route('users.create') }}">
-                        <div class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-600 focus:bg-gray-600 active:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'">Add User</div>
+                        <x-button-main>Add User</x-button-main>
                     </a>
                 </div>
             </div>
